6f0be1c63e0e893d3b39fe8eb6925c62c9e25f91
tknieza <tadasknieza@gmail.com>
2020-02-07 22:39:23 +0000

    removed placeholder icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import { CSSTransition } from "react-transition-group";
-import Home from "./components/Home";
+import Home from "./components/home";
 import Summary from "./components/summary";
 import Search from "./components/search";
 import "./App.css";
